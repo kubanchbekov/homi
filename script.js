0c811c89316354f13e4c3619cb13af0100fdbe89
Kutman <kubanchbekovkutman9@gmail.com>
2024-02-19 14:04:06 +0600

    hom work2

---
+++ script.js
@@ -0,0 +1,33 @@
+let name ; 
+console.log("My name is");
+console.log("kutman");
+console.log("My name is: kutman  ");
+console.log( 3 + 5 );
+console.log(`Total cost: $8`);
+alert ("Heloo")
+console.log((599 + 295) / 100) 
+console.log(`Total cost: $${ (599 + 295) / 100} `) 
+alert (`Total cost $${ (599 + 295 )/ 100}  Thank you, come again!`)
+console.log(`Items (4) $${ (2 * 2095 + 2 * 799) / 100} `)
+console.log(`Shipping & handling $${ (499 + 499) / 100}`)
+console.log(`Total before tax $${ (2 * 2095) + (2 * 799 ) + 499 + (499 / 100)}`)
+console.log(`Estimated tax (10%):$${Math.round((2 * 2095 + 2 * 499 + 499) * 0.1) / 100}`)
+
+
+const toki = "Simon"
+console.log('My name is '+ toki);
+console.log(`My name is ${toki}`);
+
+const foro = (5 + 2 * 3 + 9);
+console.log(foro)
+
+let qwer = foro*0.1;
+console.log(qwer)
+
+let wrer = foro+ 2;
+console.log(wrer)
+
+
+
+
+
